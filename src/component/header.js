a63b615403c291f1559519f587b42264760fec4b
subin0321 <2023028@bssm.hs.kr>
2025-01-06 11:59:00 +0900

    로그인 상태일경우 로그인 버튼 로그아웃 버튼으로 변경

--- src/component/header.js
+++ src/component/header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-const Header = ({ username }) => {
-const navigate = useNavigate();
 
-const handleStartClick = () => {
+const Header = ({ username, onLogout }) => {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
     navigate('/mainPage'); 
   };
-  
+
+  const handleLoginClick = () => {
+    navigate('/login');  
+  };
+
+  const handleSignUpClick = () => {
+    navigate('/signUp');  
+  };
+
+  const handleLogoutClick = () => {
+    // Call the onLogout function to clear the user session (e.g., remove user from state or localStorage)
+    onLogout();
+    navigate('/'); // Navigate to the home page or login page after logging out
+  };
+
   return (
     <header className="flex justify-between items-center px-6 py-4">
       {/* 왼쪽 로고 */}
@@ -28,12 +43,29 @@ const handleStartClick = () => {
           </span>
         )}
 
-        <button className="border border-blue-500 text-black bg-white px-5 py-1 mr-4 rounded-md transition duration-200 w-32">
-          회원가입
-        </button>
-        <button className="bg-blue-500 text-white px-5 py-1 mr-10 rounded-md transition duration-200 w-32">
-          로그인
-        </button>
+        {/* 회원가입 버튼 */}
+        {!username && (
+          <button 
+            onClick={handleSignUpClick} 
+            className="border border-blue-500 text-black bg-white px-5 py-1 mr-4 rounded-md transition duration-200 w-32">
+            회원가입
+          </button>
+        )}
+
+        {/* 로그인/로그아웃 버튼 */}
+        {username ? (
+          <button 
+            onClick={handleLogoutClick} 
+            className="bg-blue-500 text-white px-5 py-1 mr-10 rounded-md transition duration-200 w-32">
+            로그아웃
+          </button>
+        ) : (
+          <button 
+            onClick={handleLoginClick} 
+            className="bg-blue-500 text-white px-5 py-1 mr-10 rounded-md transition duration-200 w-32">
+            로그인
+          </button>
+        )}
       </div>
     </header>
   );
